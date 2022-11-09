cb08b13fb4b2e7c62c781d197dd08388a04cda4d
taeck <taeck007@gmail.com>
2022-11-09 10:06:59 +0900

    eslint, prettier 추가

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'standard-with-typescript',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['tsconfig.json'],
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': [
+			2,
+			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+		],
+		'@typescript-eslint/explicit-function-return-type': 'warn',
+		'@typescript-eslint/strict-boolean-expressions': 'warn',
+		'@typescript-eslint/no-floating-promises': 'warn',
+	},
+};
