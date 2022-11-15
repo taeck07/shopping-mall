f908e8fceccb7fe7b9ca338d1907b6deac30ac06
taeck <taeck007@gmail.com>
2022-11-15 20:32:39 +0900

    recoil 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ProductList } from './pages/ProductList';
 
@@ -13,15 +12,13 @@ function App() {
 		},
 	});
 	return (
-		<RecoilRoot>
-			<QueryClientProvider client={queryClient}>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<ProductList />} />
-					</Routes>
-				</BrowserRouter>
-			</QueryClientProvider>
-		</RecoilRoot>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<ProductList />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
 
